030b5bba1238fc57b3a8754dea08fdc0ca326551
François Georgy <francois.georgy@gmail.com>
2018-04-20 15:21:03 +0200

    support for async / await

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # rx-db
+
 Playing with RxJS and PouchDB
+
+## Resources
+
+Thanks to https://medium.com/@luijar/rxjs-pouchdb-persistent-data-flows-480f503ee41f
+
+## PourchDB
+
+https://pouchdb.com/guides/databases.html
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import Rx from "rxjs/Rx";
+import * as R from "ramda";
+import PouchDB from "pouchdb";
+// import Rx from "rxjs/Rx";
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
@@ -6,6 +8,47 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     document.getElementById("app").innerText = "rx-db";
 
-    // const logDb = new PouchDB("midi-log");
+    const db = new PouchDB("rx-db");
+
+    console.log(R.isNil(db.then));          //-> false
+    console.log(R.is(Function, db.then));   //-> true
+
+    async function getDbInfos() {
+        return await db.info();
+    }
+
+    console.log(getDbInfos());
+
+    /*
+    db.info().then(function(infos) {
+        console.log(JSON.stringify(infos));
+    }).catch(function (err) {
+        console.log(err);
+    });
+
+    Rx.Observable.fromEvent(db, 'created')
+        .subscribe(() => console.log("Database to accept data"));
+
+    let numbers = Rx.Observable.range(1, 10);
+    numbers.subscribe(x => console.log(x));
+    */
+
+
+    /*
+    Rx.Observable.from(getTransactionsArray())
+        .bufferCount(20)
+        .timestamp()
+        .map(objs => R.map(R.set(dateLens, objs.timestamp), objs.value))
+        .do(txs => console.log(`Processing ${txs.length} transactions`))
+        .mergeMap(datedTxs =>
+            Rx.Observable.fromPromise(txDb.bulkDocs(datedTxs)))
+        .subscribe(
+            rec => console.log(`New record created: ${rec.id}`),
+            err => console.log(‘Error: ‘ + err),
+            () => console.log(‘Database populated!’)
+        );
+    */
+
+
 
 });
