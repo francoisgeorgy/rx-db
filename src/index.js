bca64b229d1d80473df5abd3839547f6936b87cf
François Georgy <francois.georgy@gmail.com>
2018-04-20 14:28:35 +0200

    base code

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Rx from "rxjs/Rx";
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    console.log("rxdb starting...");
+
+    document.getElementById("app").innerText = "rx-db";
+
+    // const logDb = new PouchDB("midi-log");
+
+});
